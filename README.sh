363bb8488b8bb827ec6c666b77c26013733faad8
Johnew Zhang <jzhang19@gsb.columbia.edu>
2018-11-11 17:57:12 -0500

    Add files via upload

--- README.sh
+++ README.sh
@@ -8,8 +8,9 @@ $ export TEXT=examples/stories/writingPrompts
 $ python preprocess.py --source-lang wp_source --target-lang wp_target --trainpref $TEXT/train --validpref $TEXT/valid --testpref $TEXT/test --destdir data-bin/writingPrompts --padding-factor 1 --thresholdtgt 10 --thresholdsrc 10
 
 # Train the model:
-$ python train.py data-bin/writingPrompts -a fconv_self_att_wp --lr 0.25 --clip-norm 0.1 --max-tokens 1500 --lr-scheduler reduce_lr_on_plateau --decoder-attention True --encoder-attention False --criterion label_smoothed_cross_entropy --weight-decay .0000001 --label-smoothing 0 --source-lang wp_source --target-lang wp_target --gated-attention True --self-attention True --project-input True --pretrained False
+$ python train.py data-bin/writingPrompts -a fconv_self_att_wp --lr 0.25 --clip-norm 0.1 --max-tokens 1500 --lr-scheduler reduce_lr_on_plateau --decoder-attention True --encoder-attention False --criterion label_smoothed_cross_entropy --weight-decay .0000001 --label-smoothing 0 --source-lang wp_source --target-lang wp_target --gated-attention True --self-attention True --project-input True --pretrained True --pretrained-checkpoint pretrained_model/models/pretrained_checkpoint.pt
 
+$ python train.py data-bin/writingPrompts -a cs_jk_jz_model_wp --lr 0.25 --clip-norm 0.1 --max-tokens 1500 --lr-scheduler reduce_lr_on_plateau --decoder-attention True --encoder-attention False --criterion label_smoothed_cross_entropy --weight-decay .0000001 --label-smoothing 0 --source-lang wp_source --target-lang wp_target --gated-attention True --self-attention True --project-input True --pretrained False
 # Train a fusion model:
 # add the arguments: --pretrained True --pretrained-checkpoint path/to/checkpoint
 
@@ -21,6 +22,6 @@ $ python train.py data-bin/writingPrompts -a fconv_self_att_wp --lr 0.25 --clip-
 
 $ python generate.py data-bin/writingPrompts --path pretrained_model/models/pretrained_checkpoint.pt --batch-size 32 --beam 1 --sampling --sampling-topk 10 --sampling-temperature 0.8 --nbest 1 --model-overrides "{'pretrained_checkpoint':'pretrained_model/models/fusion_checkpoint.pt'}"
 
-$ python eval_lm.py data-bin/writingPrompts --path 'models/pretrained_checkpoint.pt' --task translation --max-tokens 32
+$ python eval_lm.py data-bin/writingPrompts --path 'pretrained_model/models/pretrained_checkpoint.pt' --task translation --max-tokens 32
 
-
+$ python interactive.py --path pretrained_model/models/pretrained_checkpoint.pt --beam 5 --task translation data-bin/writingPrompts
